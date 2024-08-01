7b91c05842880027c51fe546fef195e10b7864c7
PedroMidueno <pedromidueno.dev@gmail.com>
2024-08-01 17:32:04 -0600

    Add types

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,17 @@ export interface LoginParams {
   email: string
   password: string
 }
+
+export interface SignUpParams extends LoginParams {
+  firstame: string
+  lastName: string
+  confirmPassword?: string
+}
+
+export interface LoginFormState {
+  name?: string
+  lastName?: string
+  email: string
+  password: string
+  confirmPassword?: string
+}
