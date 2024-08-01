ad95e7309ad70e888dbc037471343d89437d37b6
PedroMidueno <pedromidueno.dev@gmail.com>
2024-08-01 17:25:56 -0600

    Add properties to app config

--- app.config.ts
+++ app.config.ts
@@ -7,8 +7,24 @@ export default defineAppConfig({
     },
     button: {
       variant: {
-        ghost: 'text-customViolet-950 dark:text-customViolet-50 hover:bg-customViolet-200 dark:hover:bg-customViolet-900'
+        ghost: 'text-customViolet-950 dark:text-customViolet-50 hover:bg-customViolet-200 dark:hover:bg-customViolet-900',
+        solid: 'text-customViolet-950 dark:text-white',
+        link: 'text-white dark:text-white hover:text-white hover:dark:text-white'
+      },
+      default: {
+        loadingIcon: 'i-mdi-loading'
       }
+    },
+    divider: {
+      border: {
+        base: 'border-customViolet-950 dark:border-white'
+      },
+      container: {
+        vertical: 'my-8'
+      }
+    },
+    notifications: {
+      position: 'bottom-0 right-0'
     }
   }
 })
