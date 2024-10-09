c4570116eab55ed0a102dcc69010a1a7d6abafe8
PedroMidueno <pedromidueno.dev@gmail.com>
2024-10-09 17:38:55 -0600

    Add action to shorten url publicly

--- stores/urls.ts
+++ stores/urls.ts
@@ -7,6 +7,22 @@ export const useUrlsStore = defineStore('urls', {
       const { $axios } = useNuxtApp()
       const { data } = await $axios.get(`/urls/${code}`)
       return data
+    },
+
+    async shortenPublic(longUrl: string) {
+      const { $axios } = useNuxtApp()
+      const { data } = await $axios.post('/urls/shorten-public', { longUrl })
+      return data
+    },
+
+    async shortenPrivate() {
+      const { $axios } = useNuxtApp()
+      // TODO
+    },
+
+    async shortenCustom() {
+      const { $axios } = useNuxtApp()
+      // TODO
     }
   }
 })
