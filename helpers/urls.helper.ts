1b17951d95894d90f59fc83aadf1b3886c8b76ea
PedroMidueno <pedromidueno.dev@gmail.com>
2024-10-15 18:39:19 -0600

    Add urls table columns in helper

---
+++ helpers/urls.helper.ts
@@ -0,0 +1,21 @@
+export const columns = [
+  {
+    key: 'shortCode',
+    label: 'Url'
+  },
+  {
+    key: 'longUrl',
+    label: 'Destino'
+  },
+  {
+    key: 'createdAt',
+    label: 'Fecha de creación'
+  },
+  {
+    key: 'enabled',
+    label: 'Estado'
+  },
+  {
+    key: 'actions'
+  }
+]
