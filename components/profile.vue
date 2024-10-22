6a2b25113ef208837e956f674c07034188e303dc
PedroMidueno <pedromidueno.dev@gmail.com>
2024-10-22 10:53:45 -0600

    Add logic in component to update user info

--- components/profile.vue
+++ components/profile.vue
@@ -1,9 +1,10 @@
 <template>
-  <section class="flex flex-col gap-2">
+  <section class="flex flex-col gap-1">
     <h3 class="themed-text text-2xl font-medium">
       Tu perfil
     </h3>
     <UForm
+      ref="form"
       :state="state"
       :schema="schema"
       class="flex flex-col gap-4 mb-4"
@@ -21,13 +22,18 @@
         <UInput v-model="state.email" :disabled="!editing" />
       </UFormGroup>
 
-      <div class="text-gray-500 dark:text-gray-400 italic">
-        Creación de la cuenta: 12/12/2024
+      <div class="text-gray-500 dark:text-gray-400 italic text-sm">
+        Creación de la cuenta: {{ formatDate(user?.createdAt, 'long') }}
       </div>
 
       <div class="flex justify-between items-center h-9">
-        <UCheckbox v-model="editing" label="Editar información" />
-        <UButton v-if="editing" label="Guardar" type="submit" />
+        <UCheckbox v-model="editing" label="Editar información" @update:model-value="syncUserInfo" />
+        <UButton
+          v-if="editing"
+          label="Guardar"
+          type="submit"
+          :loading="loading"
+        />
       </div>
     </UForm>
     <change-password />
@@ -36,14 +42,27 @@
 
 <script setup lang="ts">
 import { z } from 'zod'
-import type { FormSubmitEvent } from '#ui/types'
+import type { FormSubmitEvent, Form } from '#ui/types'
 
+interface ProfileState {
+  firstName: string | undefined
+  lastName: string | undefined
+  email: string | undefined
+}
+
+const authStore = useAuthStore()
+const adminStore = useAdminStore()
+const { updateUserInfo } = adminStore
+const { getUserInfo } = authStore
+const { user } = storeToRefs(authStore)
 const editing = ref(false)
-const state = reactive<Record<string, string | undefined>>({
+const state = reactive<ProfileState>({
   firstName: undefined,
   lastName: undefined,
   email: undefined
 })
+const form = ref<Form<Schema>>()
+const loading = ref(false)
 
 const schema = z.object({
   firstName: z.string({ message: 'Campo obligatorio' }).min(1, 'Campo obligatorio'),
@@ -54,6 +73,29 @@ const schema = z.object({
 type Schema = z.output<typeof schema>
 
 const onSubmit = async (event: FormSubmitEvent<Schema>) => {
-  console.log(event.data)
+  loading.value = true
+  try {
+    await updateUserInfo(event.data)
+    await getUserInfo()
+    syncUserInfo()
+    showSuccessToast('Información actualizada')
+    editing.value = false
+  } catch (error: any) {
+    showErrorToast(error.esMessage ?? 'No se pudo actualizar la información del usuario')
+    syncUserInfo()
+  } finally {
+    loading.value = false
+  }
 }
+
+const syncUserInfo = () => {
+  state.firstName = user.value?.firstName
+  state.lastName = user.value?.lastName
+  state.email = user.value?.email
+  form.value?.clear()
+}
+
+onBeforeMount(() => {
+  syncUserInfo()
+})
 </script>
