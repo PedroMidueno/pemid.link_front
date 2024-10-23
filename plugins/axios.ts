8a63eb683bd24966321b5b25044b5d7d08bd4ace
PedroMidueno <pedromidueno.dev@gmail.com>
2024-10-23 09:34:57 -0600

    Add logout action when request returns status 401

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -18,7 +18,15 @@ export default defineNuxtPlugin(() => {
   axiosInstance.interceptors.response.use(response => {
     return response.data
   }, error => {
-    return Promise.reject(error.response.data)
+    if (error.response.data.statusCode === 401 || error.response.status === 401) {
+      if (authStore.isAuthenticated) {
+        showErrorToast('Su sesión ha expirado, inicie sesión de nuevo para continuar')
+        authStore.logout()
+        return navigateTo('/login')
+      }
+    } else {
+      return Promise.reject(error.response.data)
+    }
   })
 
   return {
