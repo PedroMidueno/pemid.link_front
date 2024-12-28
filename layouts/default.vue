e46767eb43531ef724d4a1ef17c7cdf9f951da9b
PedroMidueno <pedromidueno.dev@gmail.com>
2024-12-28 15:48:58 -0600

    Add responsive to nav bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,13 @@
   <section>
     <UContainer
       as="nav"
-      class="w-full h-14 flex justify-between items-center"
+      class="h-12 flex justify-between items-center w-full sm:h-14"
     >
-      <article class="h-12 flex justify-center items-center">
+      <article class="h-10 sm:h-12 flex justify-center items-center">
         <img src="../assets/img/logo.svg" alt="Logo" class="h-full">
       </article>
 
-      <article class="h-14 flex justify-center items-center gap-4">
+      <article class="h-14 flex justify-center items-center gap-2">
         <ui-theme-switch />
         <UDropdown v-if="isAuthenticated" :items="items">
           <UButton icon="i-mdi-triangle-small-down" trailing>
@@ -16,11 +16,11 @@
           </UButton>
         </UDropdown>
 
-        <div v-else class="flex gap-4 flex-nowrap">
-          <UButton to="/login?action=signup" class="themed-button">
+        <div v-else class="hidden sm:flex sm:gap-4 flex-nowrap">
+          <UButton to="/login?action=signup">
             Registrarse
           </UButton>
-          <UButton to="/login?action=signin" class="themed-button">
+          <UButton to="/login?action=signin">
             Iniciar sesión
           </UButton>
         </div>
@@ -30,8 +30,26 @@
           to="https://github.com/PedroMidueno/pemid.link_front"
           target="blank"
         />
+        <UPopover v-model:open="showMenu" class="sm:hidden">
+          <UButton
+            variant="ghost"
+            :icon="showMenu ? 'i-mdi-close' : 'i-mdi-menu'"
+          />
+
+          <template #panel>
+            <div class="p-4 flex gap-2">
+              <UButton to="/login?action=signup" @click="showMenu = false">
+                Registrarse
+              </UButton>
+              <UButton to="/login?action=signin" @click="showMenu = false">
+                Iniciar sesión
+              </UButton>
+            </div>
+          </template>
+        </UPopover>
       </article>
     </UContainer>
+
     <slot />
   </section>
 </template>
@@ -41,6 +59,7 @@ const router = useRouter()
 const authStore = useAuthStore()
 const { user, isAuthenticated } = storeToRefs(authStore)
 const { logout } = authStore
+const showMenu = ref(false)
 
 const signOut = () => {
   logout()
